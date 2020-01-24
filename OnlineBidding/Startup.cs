1ca6d1b427ab889acf67aa2dbda5d30e2e78c811
Vlatko <vlatko.zmejkoski@it-labs.com>
2020-01-24 17:16:22 +0100

    Initial commit

---
+++ OnlineBidding/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineBidding.Startup))]
+namespace OnlineBidding
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
